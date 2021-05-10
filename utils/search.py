7fc868ee6b2985dcb9699a802a55b1bdfb59ca25
NenadPantelic <nenadpantelickg@gmail.com>
2021-05-10 12:43:47 +0200

    XEN#1039: Change elastic search index populating function.

--- utils/search.py
+++ utils/search.py
@@ -27,33 +27,15 @@ def _store_course_es_record(id, record):
 
 
 def _convert_course_object_to_es_record(course):
-    course_duration = course.video_content_length
-    try:
-        duration = float(course_duration.split()[0])
-    except:
-        duration = 0
     return {
         "_class": "com.xenecca.api.es.models.CourseDoc",
         "title": course.title,
         "doc_id": course.id,
-        "badge": course.badge,
         "headline": course.headline,
-        "price": float(course.price),
-        "old_price": float(course.old_price),
-        "price_as_string": str(course.price) + str(course.currency),
-        "avg_rating": str(course.avg_rating),
         "category": course.category.id if course.category else None,
         "subcategory": course.subcategory.id if course.subcategory else None,
-        "topic": course.topic.id if course.topic else None,
         "language": course.language.id if course.language else None,
         "poster": course.poster_path,
         "original_poster_url": course.original_poster_url,
-        "duration_in_hrs": duration,
-        "num_of_students": course.num_of_students,
-        "num_of_reviews": course.num_of_reviews,
-        "updated_at": course.updated_at.strftime("%Y-%m-%d %H:%M:%S"),
-        "instructors": [
-            {"full_name": instructor.full_name, "image": instructor.image_path}
-            for instructor in course.instructors
-        ],
+        "updated_at": course.updated_at.strftime("%Y-%m-%d %H:%M:%S")
     }
